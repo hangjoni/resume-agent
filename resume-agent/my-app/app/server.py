de7e1051300a4a4155eb149b3209da56d626d3d6
Joni <hangjoni@gmail.com>
2024-06-25 17:52:31 +0800

    protected the api

--- resume-agent/my-app/app/server.py
+++ resume-agent/my-app/app/server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException, Security, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from langchain_openai import ChatOpenAI
@@ -6,15 +7,44 @@ from langchain_community.llms import Ollama
 import pymupdf
 from .llm import resume_to_json
 
+import firebase_admin
+from firebase_admin import credentials, auth
+
+
+# protect the api by verifying the Authorizer header with Firebase
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred)
+
+security = HTTPBearer()
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Security(security)):
+    token = credentials.credentials
+    try:
+        # Verify the Firebase token
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token
+    except Exception as e:
+        raise HTTPException(status_code=403, detail="Invalid authentication credentials")
+    
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
     description="Spin up a simple api server using Langchain's Runnable interfaces",
+    dependencies=[Depends(get_current_user)]
 )
 
-@app.get("/test")
+# one public route for testing
+def no_auth_dependency():
+    return True
+
+@app.get("/test", dependencies=[Depends(no_auth_dependency)])
 async def test():
     return "Test successful!"
+
+# all other routes require authentication
+@app.get("/authtest")
+async def test():
+    return "Authentication check successful!"
 
 @app.post("/upload_pdf")
 async def upload_pdf(file: UploadFile = File(...)):
@@ -32,17 +62,17 @@ async def parse_resume_text(text: str):
     json_output = resume_to_json(text)
     return json_output
 
-add_routes(
-    app,
-    ChatOpenAI(model="gpt-3.5-turbo-0125"),
-    path="/openai",
-)
+# add_routes(
+#     app,
+#     ChatOpenAI(model="gpt-3.5-turbo-0125"),
+#     path="/openai",
+# )
 
-add_routes(
-    app,
-    Ollama(model="mistral"),
-    path="/mistral"
-)
+# add_routes(
+#     app,
+#     Ollama(model="mistral"),
+#     path="/mistral"
+# )
 
 
 if __name__ == "__main__":
